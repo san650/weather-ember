c6b3794b2bc06a9aecaccd7dd65be631c668f01b
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-11-10 22:44:28 -0200

    Add city details route and template

--- app/js/router.js
+++ app/js/router.js
@@ -1,9 +1,17 @@
 Weather.Router.map(function () {
-  this.resource("cities", { path: "/" });
+  this.resource("cities", { path: "/" }, function(){
+    this.route("details", { path: "/:city_id" });
+  });
 });
 
 Weather.CitiesRoute = Ember.Route.extend({
   model: function() {
     return this.store.find('city');
+  }
+});
+
+Weather.CitiesDetailsRoute = Ember.Route.extend({
+  model: function(params) {
+    return this.store.findById('city', params.city_id);
   }
 });
