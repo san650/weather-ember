b0d9b31455f0d4788620df5eb9b03f001d04963a
Adrian Mugnolo <adrian@wyeworks.com>
2013-11-12 11:01:33 -0200

    List cities from fixtures

---
+++ app/js/router.js
@@ -0,0 +1,9 @@
+Weather.Router.map(function () {
+  this.resource('cities', { path: '/' });
+});
+
+Weather.CitiesRoute = Ember.Route.extend({
+  model: function () {
+    return this.store.find('city');
+  }
+});
