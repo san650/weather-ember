d215dcf6ce1cbd7b63ed9f8de80d5ccf4b7a35ed
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-11-22 08:18:20 -0200

    Add cities route to the router

--- app/js/application.js
+++ app/js/application.js
@@ -1,6 +1,14 @@
+// ## Application
+
 window.Weather = Ember.Application.create({
   ready: function(){
     console.log("Application ready!");
   },
   LOG_TRANSITIONS: true
+});
+
+// ## ROUTES
+
+Weather.Router.map(function() {
+  this.route("cities");
 });
