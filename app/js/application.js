0eb9f6ac2a49691bb5e640f8992152d13b45c442
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2013-11-24 11:56:40 -0200

    Sort cities and show total using a controller

--- app/js/application.js
+++ app/js/application.js
@@ -25,3 +25,21 @@ Weather.CitiesRoute = Ember.Route.extend({
     return this.store.find('city');
   }
 });
+
+// ## Controllers
+
+Weather.CitiesController = Ember.ArrayController.extend({
+  sortProperties: ['name'],
+  sortAscending: true,
+
+  total: function() {
+    switch(this.get("length")) {
+      case 0:
+        return "There are no cities";
+      case 1:
+        return "There is one city";
+      default:
+        return "There are " + this.get("length") + " cities";
+    }
+  }.property("length")
+});
